5a9043c59d3df0c6614d94d4c764a51a295a5c88
godwynnn <78883893+godwynnn@users.noreply.github.com>
2022-03-10 12:39:07 +0100

    Update settings.py

--- ekere/settings.py
+++ ekere/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ekere-ecomm.herokuapp.com','127.0.0.1']
 # APPEND_SLASH=False
 
 
